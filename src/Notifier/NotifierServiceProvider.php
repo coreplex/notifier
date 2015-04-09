7956572ecfc21ae40824cd0323cb761809eff481
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2015-04-09 17:19:44 +0100

    Moved the config and public folders to the route and updated paths

--- src/Notifier/NotifierServiceProvider.php
+++ src/Notifier/NotifierServiceProvider.php
@@ -16,14 +16,14 @@ class NotifierServiceProvider extends ServiceProvider {
     public function boot()
     {
         // Set the directory to load views from
-        $this->loadViewsFrom(__DIR__.'/../public/views/', 'notifier');
+        $this->loadViewsFrom(__DIR__.'/../../public/views/', 'notifier');
         // Set the folders to publish
         $this->publishes([
-            __DIR__.'/../config/notifier.php' => config_path('notifier.php'),
-            __DIR__.'/../public/views/' => public_path('resources/views/'),
+            __DIR__.'/../../config/notifier.php' => config_path('notifier.php'),
+            __DIR__.'/../../public/views/' => public_path('resources/views/'),
         ]);
         // Merge the original config with the applications version.
-        $this->mergeConfigFrom(__DIR__.'/../config/notifier.php', 'notifier');
+        $this->mergeConfigFrom(__DIR__.'/../../config/notifier.php', 'notifier');
     }
 
     /**
