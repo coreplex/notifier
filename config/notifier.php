6b3dd6c82ac683e02de9636842658e162939b72a
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2015-04-10 15:10:56 +0100

    Updated the comments in the config

--- config/notifier.php
+++ config/notifier.php
@@ -12,21 +12,39 @@ return [
      */
     'driver' => 'growl',
 
-
+    /**
+     * --------------------------------------------------------------------
+     *  Views
+     * --------------------------------------------------------------------
+     * 
+     * Set the views to be used by templatable renderers.
+     */
     'views' => [
 
         'growl' => [
 
+            /**
+             * The view to render a single notification.
+             */
             'notification' => 'notifier::growl.notification',
 
+            /**
+             * The view to renderer all of the rendered noticiations.
+             */
             'template' => 'notifier::growl.default',
 
         ],
 
         'alertify' => [
 
+            /**
+             * The view to render a single notification.
+             */
             'notification' => 'notifier::alertify.notification',
 
+            /**
+             * The view to renderer all of the rendered noticiations.
+             */
             'template' => 'notifier::alertify.default',
 
         ]
