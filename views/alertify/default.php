25b2a1522dfa5f9bb7b4ac8799a332901c1ac1a8
Michael Jennings <michael.jennings91@gmail.com>
2015-09-19 12:18:28 +0100

    Removed the views folder

--- views/alertify/default.php
+++
@@ -1,5 +0,0 @@
-<?php foreach ($notifier->getNotifications() as $level => $notifications): ?>
-    <?php foreach ($notifications as $notification): ?>
-        <?php echo $notification ?>
-    <?php endforeach; ?>
-<?php endforeach; ?>
--- views/growl/notification.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-    $.growl<?php echo $notification->growlLevel; ?>({ title: "<?php echo  $notification->title; ?>", message: "<?php echo $notification->message; ?>" });
-</script>
