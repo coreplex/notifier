6a8fbbd392a5b7186a7bebd9760048314af226aa
Michael Jennings <michael.jennings91@gmail.com>
2015-10-06 12:00:53 +0100

    Update parser to run different if statements

--- src/Parser.php
+++ src/Parser.php
@@ -51,7 +51,9 @@ class Parser implements TemplateParser
     protected function parseIfs($body, array $replacements)
     {
         foreach ($this->getIfs($body) as $if) {
-            $body = str_replace($if[0], ! isset($replacements[$if[1]]) ? "" : $if[2], $body);
+            $check = $this->check($if[1], $replacements);
+
+            $body = str_replace($if[0], $check ? $if[2] : "", $body);
         }
 
         return $body;
@@ -89,5 +91,144 @@ class Parser implements TemplateParser
         }
 
         return false;
+    }
+
+    /**
+     * Check if the provided expression is true.
+     *
+     * @param string $expression
+     * @param array  $data
+     * @return bool
+     */
+    protected function check($expression, array $data)
+    {
+        if (strpos($expression, '==') !== false) {
+            return $this->checkEquals($expression, $data);
+        }
+
+        if (strpos($expression, '!=') !== false) {
+            return $this->checkDoesNotEqual($expression, $data);
+        }
+
+        if (strpos($expression, '>=') !== false) {
+            return $this->checkGreaterThanOrEqual($expression, $data);
+        }
+
+        if (strpos($expression, '>') !== false) {
+            return $this->checkGreaterThan($expression, $data);
+        }
+
+        if (strpos($expression, '<=') !== false) {
+            return $this->checkLessThanOrEqual($expression, $data);
+        }
+
+        if (strpos($expression, '<') !== false) {
+            return $this->checkLessThan($expression, $data);
+        }
+
+        return isset($expression);
+    }
+
+    /**
+     * Run the equals if statement.
+     *
+     * @param string $expression
+     * @param array  $data
+     * @return bool
+     */
+    protected function checkEquals($expression, array $data)
+    {
+        $args = explode('==', $expression);
+        list($key, $value) = $this->cleanKeyValue($args);
+
+        return isset($data[$key]) ? ($data[$key] == $value) : false;
+    }
+
+    /**
+     * Run the does not equal statement.
+     *
+     * @param string $expression
+     * @param array  $data
+     * @return bool
+     */
+    protected function checkDoesNotEqual($expression, array $data)
+    {
+        $args = explode('!=', $expression);
+        list($key, $value) = $this->cleanKeyValue($args);
+
+        return isset($data[$key]) ? ($data[$key] != $value) : false;
+    }
+
+    /**
+     * Run the greater than statement.
+     *
+     * @param string $expression
+     * @param array  $data
+     * @return bool
+     */
+    protected function checkGreaterThan($expression, array $data)
+    {
+        $args = explode('>', $expression);
+        list($key, $value) = $this->cleanKeyValue($args);
+
+        return isset($data[$key]) ? ($data[$key] > $value) : false;
+    }
+
+    /**
+     * Run the greater than or equal to statement.
+     *
+     * @param string $expression
+     * @param array  $data
+     * @return bool
+     */
+    protected function checkGreaterThanOrEqual($expression, array $data)
+    {
+        $args = explode('>=', $expression);
+        list($key, $value) = $this->cleanKeyValue($args);
+
+        return isset($data[$key]) ? ($data[$key] >= $value) : false;
+    }
+
+    /**
+     * Run the less than statement.
+     *
+     * @param string $expression
+     * @param array  $data
+     * @return bool
+     */
+    protected function checkLessThan($expression, array $data)
+    {
+        $args = explode('<', $expression);
+        list($key, $value) = $this->cleanKeyValue($args);
+
+        return isset($data[$key]) ? ($data[$key] < $value) : false;
+    }
+
+    /**
+     * Run the less than or equal to statement.
+     *
+     * @param string $expression
+     * @param array  $data
+     * @return bool
+     */
+    protected function checkLessThanOrEqual($expression, array $data)
+    {
+        $args = explode('<=', $expression);
+        list($key, $value) = $this->cleanKeyValue($args);
+
+        return isset($data[$key]) ? ($data[$key] <= $value) : false;
+    }
+
+    /**
+     * Get the key value pair from the provided array.
+     *
+     * @param array $args
+     * @return array
+     */
+    protected function cleanKeyValue($args)
+    {
+        $key = trim($args[0]);
+        $value = str_replace(['\'', '"'], '', trim($args[1]));
+        return array($key, $value);
     }
 }
